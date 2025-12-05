b2ac7229885c1da9ae6f1e225ae13cdb15ccb729
scratcheurs25 <126276357+scratcheurs25@users.noreply.github.com>
2025-12-05 22:15:50 +0100

    Refactor result handling and update database schema

--- date_base_cubing/result.py
+++ date_base_cubing/result.py
@@ -10,8 +10,8 @@ def init_db():
     cur.execute("""CREATE TABLE IF NOT EXISTS result (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             time INT NOT NULL,
-            id_event_group INTEGER NOT NULL,
-            user TEXT NOT NULL)""")
+            id_event INTEGER NOT NULL,
+            user_id TEXT NOT NULL)""")
     conn.commit()
     conn.close()
 init_db()
@@ -19,7 +19,7 @@ init_db()
 def get_result(id):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM groupe_event WHERE id = ?""", (id,))
+    cur.execute("""SELECT * FROM result WHERE id = ?""", (id,))
 
     rows = cur.fetchall()
 
@@ -27,22 +27,22 @@ def get_result(id):
 
     # rows est une liste de Row, on la convertit en liste de dict
     return [dict(r) for r in rows]
-def add_result( id_event , id_group ):
+def add_result( time , id_event , user ):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("""INSERT INTO groupe_event (id_event ,id_group) VALUES (?, ?);""",
-                (id_event, id_group))
+    cur.execute("""INSERT INTO result (time , id_event ,user_id) VALUES (?, ? ,?);""",
+                (time , id_event, user))
 
     conn.commit()
     new_id = cur.lastrowid
     conn.close()
 
-    return {"id": new_id, "id_user": id_event, "id_group": id_group}
+    return {"id": new_id, "time": time, "id_event": id_event , "user_id" : user}
 def del_result(id):
     conn = get_connection()
     cur = conn.cursor()
     try:
-        cur.execute("DELETE FROM groupe_event WHERE id = ?;", (id,))
+        cur.execute("DELETE FROM result WHERE id = ?;", (id,))
 
         conn.commit()
         new_id = cur.lastrowid
@@ -53,7 +53,15 @@ def del_result(id):
 def get_all_result():
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("""select * from groupe_event;""")
+    cur.execute("""select * from result;""")
+    rows = cur.fetchall()
+    conn.close()
+    return [dict(r) for r in rows]
+def get_all_result_from_user_in_event(event_id,user_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""   Select * from result where user_id = ? and id_event = ?  """, (user_id , event_id))
     rows = cur.fetchall()
     conn.close()
     return [dict(r) for r in rows]
